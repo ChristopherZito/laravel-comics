0f58d0a648be68cb819a59a23fefd3f5805bb8ba
christopherzito <christopherzito11@gmail.com>
2022-01-21 18:08:50 +0100

    upgrade page single-comic

--- resources/views/pages/comicsOne.blade.php
+++ resources/views/pages/comicsOne.blade.php
@@ -35,11 +35,70 @@
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, modi. Quisquam tempore animi ducimus est. Aperiam id laudantium ipsum obcaecati ab voluptates veritatis repellat, odio neque voluptate impedit velit beatae.
             </p>
         </section>
+        <section class="more-info">
+            {{-- more info for the comic --}}
+            <div class="display-flex-v">
+                <div class="sub-display">
+                    <h2>
+                        Talent
+                    </h2>
+                    <hr>
+                    <div class="text-display">
+                        <span>
+                            Art by:
+                        </span>
+                        <p class="text-blu">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illum culpa quos perspiciatis tenetur laudantium incidunt pariatur repellat. Vero dolore laboriosam pariatur dignissimos. Necessitatibus labore sed impedit sapiente sit eveniet.
+                        </p>
+                    </div>
+                    <hr>
+                    <div class="text-display"> 
+                        <span>
+                            Written by:
+                        </span>
+                        <p class="text-blu">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illum culpa quos perspiciatis tenetur laudantium incidunt pariatur repellat. Vero dolore laboriosam pariatur dignissimos. Necessitatibus labore sed impedit sapiente sit eveniet.
+                        </p>
+                    </div>
+                    <hr>
+                </div>
+                <div class="sub-display">
+                    <h2>
+                        Specs
+                    </h2>
+                    <hr>
+                    <div class="text-display">
+                        <span>
+                            Series:
+                        </span>
+                        <p class="text-blu">
+                            ACTION COMICS
+                        </p>
+                    </div>
+                    <hr>
+                    <div class="text-display">
+                        <span>
+                            U.S. Price:
+                        </span>
+                        <p>
+                            $19.99
+                        </p>
+                    </div>
+                    <hr>
+                    <div class="text-display">
+                        <span>
+                            On Sale Date:
+                        </span>
+                        <p>
+                            oct 02 2018
+                        </p>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </section>
+        <section class="sub-shop">
+            {{-- shop grey --}}
+        </section>
     </div>
-    <section>
-        {{-- more info for the comic --}}
-    </section>
-    <section>
-        {{-- shop grey --}}
-    </section>
 @endsection
