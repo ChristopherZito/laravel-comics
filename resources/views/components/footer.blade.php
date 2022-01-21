04090fdf667ed54173c5a8a702d96774c47c86cc
christopherzito <christopherzito11@gmail.com>
2022-01-21 15:57:27 +0100

    footer, header fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,30 @@
 <footer>
-    <div>
-        <h1>
-            DC COMICS
-        </h1>
-        <h1>
-            SHOP
-        </h1>
-        <h1>
-            DC
-        </h1>
-        <h1>
-            SITES
-        </h1>
-    </div>
-    <div>
-        <button>
+    <section>
+        <div id="info">
+            <h1>
+                DC COMICS
+            </h1>
+            <h1>
+                SHOP
+            </h1>
+            <h1>
+                DC
+            </h1>
+            <h1>
+                SITES
+            </h1>
+        </div>
+    </section>
+    <section>
+        <div id="log">
+            <button>
+                <h2>
+                    SING-UP NOW!
+                </h2>
+            </button>
             <h2>
-                SING-UP NOW!
+                FOLLOW US
             </h2>
-        </button>
-        <h2>
-            FOLLOW US
-        </h2>
-    </div>
+        </div>
+    </section>
 </footer>
