273423ee86c029728295548fcbc4c5e0ad4d5a9c
christopherzito <christopherzito11@gmail.com>
2022-01-21 15:38:58 +0100

    header, soft-footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,26 @@
 <footer>
-    footer
+    <div>
+        <h1>
+            DC COMICS
+        </h1>
+        <h1>
+            SHOP
+        </h1>
+        <h1>
+            DC
+        </h1>
+        <h1>
+            SITES
+        </h1>
+    </div>
+    <div>
+        <button>
+            <h2>
+                SING-UP NOW!
+            </h2>
+        </button>
+        <h2>
+            FOLLOW US
+        </h2>
+    </div>
 </footer>
