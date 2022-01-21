8fb49f2e6c2aa2771f7e5326553d18bbdde07c01
christopherzito <christopherzito11@gmail.com>
2022-01-21 14:45:33 +0100

    layout idea

---
+++ resources/views/pages/body.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main-layout')
+@section('content')
+    <h1>
+        Comics collection
+    </h1>
+@endsection
---
+++ resources/views/pages/comicsOne.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main-layout')
+@section('content')
+    <h1>
+        First Comic
+    </h1>
+@endsection
