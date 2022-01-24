6846f9d7a0255226c06cf49d9e4b3f7715a2afca
christopherzito <christopherzito11@gmail.com>
2022-01-24 14:07:57 +0100

    footer update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,107 +1,116 @@
 <footer>
-    <section>
+    <section id="foot-jumbotron">
         <div id="info">
-            <div>
-                <h1>
-                    DC COMICS
-                </h1>
-                <ul>
-                    <li>Characters
-                    </li>
-                    <li>
-                        Comics
-
-                    </li>
-                    <li>
-                        movies
-
-                    </li>
-                    <li>
-                        TV
-                    </li>
-                    <li>
-                        Games
-                    </li>
-                    <li>
-                        Videos
-                    </li>
-                    <li>
-                        News 
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h1>
-                    SHOP
-                </h1>
-                <ul>
-                    <li>
-                        Shop DC
-                    </li>
-                    <li>
-                        Shop DC Collectibles
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h1>
-                    DC
-                </h1>
-                <ul>
-                    <li>
-                        Terms Of Use
-                    </li>
-                    <li>
-                        Privacy policy (New)
-                    </li>
-                    <li>
-                        Ad Choices
-                    </li>
-                    <li>
-                        Adcertising
-                    </li>
-                    <li>
-                        Jobs
-                    </li>
-                    <li>
-                        Subsriptions
-                    </li>
-                    <li>
-                        Talent Worckshops
-                    </li>
-                    <li>
-                        CPSC Certificate
-                    </li>
-                    <li>
-                        Rating
-                    </li>
-                    <li>
-                        Shop Help
-                    </li>
-                    <li>
-                        Contact Us
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h1>
-                    SITES
-                </h1>
-                <ul>
-                    <li>
+            <section>
+                {{-- info --}}
+                <div>
+                    <h1>
+                        DC COMICS
+                    </h1>
+                    <ul>
+                        <li>Characters
+                        </li>
+                        <li>
+                            Comics
+    
+                        </li>
+                        <li>
+                            movies
+    
+                        </li>
+                        <li>
+                            TV
+                        </li>
+                        <li>
+                            Games
+                        </li>
+                        <li>
+                            Videos
+                        </li>
+                        <li>
+                            News 
+                        </li>
+                    </ul>
+                    <div>
+                        <h1>
+                            SHOP
+                        </h1>
+                        <ul>
+                            <li>
+                                Shop DC
+                            </li>
+                            <li>
+                                Shop DC Collectibles
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h1>
                         DC
-                    </li>
-                    <li>
-                        MAD Magazine
-                    </li>
-                        DC Kids
-                    <li>
-                    </li>
-                        DC Universe
-                    <li>
-                        DC Power Visa
-                    </li>
-                </ul>
+                    </h1>
+                    <ul>
+                        <li>
+                            Terms Of Use
+                        </li>
+                        <li>
+                            Privacy policy (New)
+                        </li>
+                        <li>
+                            Ad Choices
+                        </li>
+                        <li>
+                            Adcertising
+                        </li>
+                        <li>
+                            Jobs
+                        </li>
+                        <li>
+                            Subsriptions
+                        </li>
+                        <li>
+                            Talent Worckshops
+                        </li>
+                        <li>
+                            CPSC Certificate
+                        </li>
+                        <li>
+                            Rating
+                        </li>
+                        <li>
+                            Shop Help
+                        </li>
+                        <li>
+                            Contact Us
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>
+                        SITES
+                    </h1>
+                    <ul>
+                        <li>
+                            DC
+                        </li>
+                        <li>
+                            MAD Magazine
+                        </li>
+                        <li>
+                            DC Kids
+                        </li>
+                        <li>
+                            DC Universe
+                        </li>
+                        <li>
+                            DC Power Visa
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            {{-- img --}}
+            <div>
+                <img src="{{asset('storage/assets/dc-logo-bg.png')}}" alt="">
             </div>
         </div>
     </section>
@@ -112,9 +121,28 @@
                     SING-UP NOW!
                 </h2>
             </button>
-            <h2>
-                FOLLOW US
-            </h2>
+            <div>
+                <h2>
+                    FOLLOW US
+                </h2>
+                <div>
+                    <div>
+                        <img src="{{asset('storage/assets/footer-facebook.png')}}" alt="">
+                    </div>
+                    <div>
+                        <img src="{{asset('storage/assets/footer-twitter.png')}}" alt="">
+                    </div>
+                    <div>
+                        <img src="{{asset('storage/assets/footer-youtube.png')}}" alt="">
+                    </div>
+                    <div>
+                        <img src="{{asset('storage/assets/footer-pinterest.png')}}" alt="">
+                    </div>
+                    <div>
+                        <img src="{{asset('storage/assets/footer-periscope.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </footer>
